9c485dad2a7befe31e18d789adca5543054713c8
Chris Keller <xylo04@gmail.com>
2025-01-19 21:56:34 -0700

    Regenerate OpenAPI Client (#112)

--- unmarshal.go
+++ unmarshal.go
@@ -11,12 +11,13 @@ func unmarshalForm(payload []byte, v interface{}) error {
 	// QRZ's Logbook API returns something resembling application/x-www-form-urlencoded, but
 	// the fields are not escaped so attempting to use url.ParseQuery() directly fails.
 	// TODO: This parser is a terrible hack and if you can think of something better, have at it.
-	findField(&s, regexp.MustCompile(`(^|&)RESULT=(\w*)`), &resp.RESULT)
-	findField(&s, regexp.MustCompile(`(^|&)REASON=(\w*)`), &resp.REASON)
-	findField(&s, regexp.MustCompile(`(^|&)LOGIDS=([\d,]*)`), &resp.LOGIDS)
-	findField(&s, regexp.MustCompile(`(^|&)LOGID=(\d*)`), &resp.LOGID)
-	findField(&s, regexp.MustCompile(`(^|&)COUNT=(\d*)`), &resp.COUNT)
-	resp.DATA = strings.Trim(s, "& \n")
+	findField(&s, regexp.MustCompile(`(^|&)RESULT=(\w*)`), resp.RESULT)
+	findField(&s, regexp.MustCompile(`(^|&)REASON=(\w*)`), resp.REASON)
+	findField(&s, regexp.MustCompile(`(^|&)LOGIDS=([\d,]*)`), resp.LOGIDS)
+	findField(&s, regexp.MustCompile(`(^|&)LOGID=(\d*)`), resp.LOGID)
+	findField(&s, regexp.MustCompile(`(^|&)COUNT=(\d*)`), resp.COUNT)
+	trim := strings.Trim(s, "& \n")
+	resp.DATA = &trim
 	return nil
 }
 
